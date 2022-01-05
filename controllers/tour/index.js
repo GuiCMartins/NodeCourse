cb140fca9868e4a98506816dffd4b832926b5f32
GuiCMartins <guilhermecmartins99@gmail.com>
2022-01-04 21:54:09 -0300

    Pagination, sort and fieldLimit

--- controllers/tour/index.js
+++ controllers/tour/index.js
@@ -3,11 +3,32 @@ const { TourModel } = require("../../models");
 const useTourController = () => {
   const getAllTours = async (req, res) => {
     try {
-      
       let queryStr = JSON.stringify(req.query);
-      queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-      
-      const query = await TourModel.find(JSON.parse(queryStr));
+      queryStr = queryStr.replace(
+        /\b(gte|gt|lte|lt)\b/g,
+        (match) => `$${match}`
+      );
+
+      const sort = req.query.sort ? req.query.sort : "-createdAt";
+      const fields = req.query.fields
+        ? req.query.fields.split(".").join(" ")
+        : "-__v";
+      const page = Number(req.query.page) || 1;
+      const limit = Number(req.query.limit) || 5;
+      const skip = (page - 1) * limit;
+
+      if (req.query.page) {
+        const numberOfTours = await TourModel.count();
+        if (skip >= numberOfTours) {
+          throw new Error("This page does not exist!");
+        }
+      }
+
+      let query = await TourModel.find(JSON.parse(queryStr))
+        .sort(sort)
+        .select(fields)
+        .skip(skip)
+        .limit(limit);
 
       const tours = query;
 
